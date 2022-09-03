a66f29c54c400fef522a903fa441779c418bfd2b
JosueRenteria <92546462+JosueRenteria@users.noreply.github.com>
2022-09-03 13:26:08 -0500

    Introduccion a Python

---
+++ Curso de Python.py
@@ -0,0 +1,14 @@
+#Titulo Programa: Documentacion de Python
+#Fecha: 03-Septiembre-2022
+#Autor: Renteria Arriaga Josue
+
+#Es un lenguaje de alto nivel, bueno para aplicaciones que interactuan con el usuario
+#Es un lenjuaje de programacion dinamico.
+#Se pueden hacer aplicaciones de linea de comandos, para aplicaciones web django.
+#Es bueno para Ciencia de Datos o la Tecnologia.
+
+#Para ver la version de python y entrar en el Simbolo del Sistema. >>python --version >>python
+#Salir del Cmd >>exit()
+
+#Para abrir un archivo de Python
+# >>python HolaMundo.py
---
+++ HolaMundo.py
@@ -0,0 +1,6 @@
+#Titulo Programa: Hola Mundo
+#Fecha: 03-Septiembre-2022
+#Autor: Renteria Arriaga Josue
+
+#Este codigo escribe un mensaje por pantalla.
+print("Hola Mundo")
